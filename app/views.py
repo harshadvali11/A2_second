f1bb403808222f27c681553a7dd8d6e72b124cb4
jspmern <jspmern@gmail.com>
2024-11-25 14:31:51 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+ 
+def atchyuth(request):
+    return HttpResponse('atchyuth Is A Innocent Boy who doesnt know Flirt')
+
